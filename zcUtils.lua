cdb5a003ba7a0f9a6bde42f7c4705c0146054c3c
Richard Steininger <richi.steininger@hotmail.com>
2023-04-09 01:11:26 +0200

    Fix colored itemtoolip text

--- zcUtils.lua
+++ zcUtils.lua
@@ -774,9 +774,9 @@ end
 --local goldicon		= "|TInterface\\MoneyFrame\\UI-GoldIcon:12:12:4:0|t"
 --local silvericon	= "|TInterface\\MoneyFrame\\UI-SilverIcon:12:12:4:0|t"
 --local coppericon	= "|TInterface\\MoneyFrame\\UI-CopperIcon:12:12:4:0|t"
-local goldicon = [[|cffffffff|r|TInterface\AddOns\ItemPriceTooltip\Textures\Gold:0:0:1|t]]
-local silvericon = [[|cffffffff|r|TInterface\AddOns\ItemPriceTooltip\Textures\Silver:0:0:1|t]]
-local coppericon = [[|cffffffff|r|TInterface\AddOns\ItemPriceTooltip\Textures\Copper:0:0:1|t]]
+local goldicon = [[|TInterface\AddOns\ItemPriceTooltip\Textures\Gold:0:0:1|t]]
+local silvericon = [[|TInterface\AddOns\ItemPriceTooltip\Textures\Silver:0:0:1|t]]
+local coppericon = [[|TInterface\AddOns\ItemPriceTooltip\Textures\Copper:0:0:1|t]]
 
 -----------------------------------------
 
